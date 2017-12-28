b7f26bc54bf3850cde2b9ac204a0d98bd53406eb
Vik <changer_5@bk.ru>
2017-12-28 19:46:38 +0300

    portfolio

---
+++ dream_auto/js/common.js
@@ -0,0 +1,59 @@
+$(document).ready(function() {
+
+
+	$(".owl-carousel").owlCarousel({
+		items: 8,
+		margin: 30,
+		loop: true,
+		navText: ['<span class="glyphicon glyphicon-chevron-left"></span>','<span class="glyphicon glyphicon-chevron-right"></span>'],
+		responsive: {
+			1000:{
+				items: 8
+			},
+			900:{
+				items: 7
+			},
+			800:{
+				items: 6
+			},
+			600:{
+				items: 5
+			},
+			480:{
+				items: 4
+			},
+			320:{
+				items: 3
+			}
+		}
+	});
+
+	$("a[href*='#home'], a[href*='#about'], a[href*='#services'], a[href*='#works'], a[href*='#partners'], a[href*='#contacts'], a[href*='#map']").mPageScroll2id();
+
+
+	$('.popup-btn').magnificPopup();
+
+	$('.image-popup-no-margins').magnificPopup({
+		type: 'image',
+		closeOnContentClick: true,
+		closeBtnInside: false,
+		fixedContentPos: true,
+		mainClass: 'mfp-no-margins mfp-with-zoom', // class to remove default margin from left and right side
+		image: {
+			verticalFit: true
+		},
+		zoom: {
+			enabled: true,
+			duration: 300 // don't foget to change the duration also in CSS
+		}
+	});
+
+	$('.mc_item').matchHeight();
+
+	// $('.navbar-header .m-hamburger').toggleClass('is-active');
+
+	// $(".navbar-toggle").on("click", function () {
+	// 	$(this).toggleClass("active");
+	// });
+
+});
---
+++ tajam/js/common.js
@@ -0,0 +1,67 @@
+$(document).ready(function() {
+
+	
+  $('.owl-carousel').owlCarousel({
+  	dots: true,
+  	items:1,
+  	loop: true,
+  	autoplay: true, 
+  	smartSpeed: 1000
+  });
+
+  $('.service_item').equalHeights();
+  $('.item').equalHeights();
+
+
+  $('.portfolio_item').magnificPopup({
+		delegate: 'a',
+		type: 'image',
+		tLoading: 'Loading image #%curr%...',
+		mainClass: 'mfp-img-mobile',
+		gallery: {
+			enabled: true,
+			navigateByImgClick: true,
+			preload: [0,1] // Will preload 0 - before current, and 1 after the current image
+		},
+		image: {
+			tError: '<a href="%url%">The image #%curr%</a> could not be loaded.',
+			titleSrc: function(item) {
+				return item.el.attr('title') + '<small>by Marsel Van Oosten</small>';
+			}
+		}
+	});
+
+
+  	$('.carousel').carousel({
+	  interval: 20000
+	});
+
+
+	$().stellar();
+
+
+	$(window).scroll(function(){
+		if ($(this).scrollTop() > $(this).height()) {
+			$('.top').addClass('active');
+		} else{
+			$('.top').removeClass('active');
+		}
+	});
+
+
+	$("a[href*='#home']").mPageScroll2id();
+	$("a[href*='#about']").mPageScroll2id();
+	$("a[href*='#services']").mPageScroll2id();
+	$("a[href*='#teams']").mPageScroll2id();
+	$("a[href*='#works']").mPageScroll2id();
+	$("a[href*='#people_say']").mPageScroll2id();
+	$("a[href*='#contacts']").mPageScroll2id();
+
+
+	
+
+});
+
+$(window).on('load', function(){
+	$('.preloader').delay(100).fadeOut('slow');
+});
