ad52b661a8d5515cd7da03b0c91e25b7c85ff4a7
Vik <changer_5@bk.ru>
2018-02-09 10:02:59 +0300

    add alexander-shop

---
+++ alexander/js/common.js
@@ -0,0 +1,14 @@
+$(document).ready(function(){
+
+  $(".owl-carousel").owlCarousel({
+  	items: 1,
+  	dots: true
+  });
+
+  $('.category-item').equalHeights();
+  $('.equalHeight').equalHeights();
+  $('.equal').equalHeights();
+});
+   
+
+
