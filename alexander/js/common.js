a53faeb60856ca2bb5ed227482239c79cd64f4ac
Vik <changer_5@bk.ru>
2018-02-19 15:29:38 +0300

    uploaded

--- alexander/js/common.js
+++ alexander/js/common.js
@@ -9,7 +9,20 @@ $(document).ready(function(){
   $('.equalHeight').equalHeights();
   $('.equal').equalHeights();
   $('.boutique-height').equalHeights();
+  $('.dashboard').equalHeights();
+  $('.service-item').equalHeights();
+  $('.service-item__defination').equalHeights();
+  $('.wrapper .foods .foods-by-profession .prof-item__header').equalHeights();
   
+  $("#select-size").selecter({
+  		customClass: "size",
+        label: 'Select Size'
+    });
+
+  $("#select-color").selecter({
+  		customClass: "color",
+        label: 'Select Color'
+    });
 
 });
    
